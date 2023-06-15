f466292182f97d487f3d9a3388f68b1330b545ef
Nick Volynkin <nick.volynkin@gmail.com>
2023-06-15 12:40:49 +0300

    ci: test the proof-market-toolchain part of workflow
    * Build a statement file from the circuit using toolchain scripts:
      ```
      scripts/ci.sh --docker build_statement
      ```
    
    * Prove & verify this file with `proof-producer`.
      ```
      scripts/ci.sh --docker prove
      ```
      This part requires input in the JSON format, see `./src/main-input.json`.
    
    * Both steps use the new Docker image with precompiled Proof Market
      toolchain: `ghcr.io/nilfoundation/proof-market-toolchain:compiled`.
    
    Resolves #19

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -24,9 +24,11 @@ check_file_exists() {
         exit 1
     else
         echo "File $FILE1 created successfully"
+        ls -hal "$FILE1"
     fi
 }
 
+# Compile source code into a circuit
 compile() {
     if [ "$USE_DOCKER" = true ] ; then
         cd "$REPO_ROOT"
@@ -49,6 +51,9 @@ compile() {
     fi
 }
 
+# Use assigner to produce a constraint file and an assignment table.
+# This is not a part of the basic development workflow,
+# but can be used for debugging circuits.
 run_assigner() {
     if [ "$USE_DOCKER" = true ] ; then
         cd "$REPO_ROOT"
@@ -74,6 +79,62 @@ run_assigner() {
     fi
   }
 
+# Use the Proof Market toolchain to pack circuit into a statement
+# that can later be used to produce a proof locally or sent to the
+# Proof Market.
+build_statement() {
+    if [ "$USE_DOCKER" = true ] ; then
+        cd "$REPO_ROOT"
+        $DOCKER run $DOCKER_OPTS \
+          --rm \
+          --platform=linux/amd64 \
+          --user $(id -u ${USER}):$(id -g ${USER}) \
+          --volume $(pwd):/opt/zkllvm-template \
+          ghcr.io/nilfoundation/proof-market-toolchain:latest \
+          sh -c "bash /opt/zkllvm-template/scripts/ci.sh build_statement"
+        cd -
+    else
+        python3 /proof-market-toolchain/scripts/prepare_statement.py \
+            --circuit "$REPO_ROOT/build/src/template.ll" \
+            --name template --type placeholder-zkllvm \
+            --output "$REPO_ROOT/build/template.json"
+        check_file_exists "$REPO_ROOT/build/template.json"
+    fi
+}
+
+# Prove the circuit with particular input.
+# See the input files at:
+# ./src/main.inp
+# ./src/main-input.json
+prove() {
+    if [ "$USE_DOCKER" = true ] ; then
+        cd "$REPO_ROOT"
+        $DOCKER run $DOCKER_OPTS \
+          --rm \
+          --platform=linux/amd64 \
+          --user $(id -u ${USER}):$(id -g ${USER}) \
+          --volume $(pwd):/opt/zkllvm-template \
+          ghcr.io/nilfoundation/proof-market-toolchain:latest \
+          sh -c "bash /opt/zkllvm-template/scripts/ci.sh prove"
+        cd -
+    else
+        # workaround for https://github.com/NilFoundation/proof-market-toolchain/issues/61
+        mkdir -p .config
+        touch .config/config.ini
+        proof-generator \
+            --circuit_input="$REPO_ROOT/build/template.json" \
+            --public_input="$REPO_ROOT/src/main-input.json" \
+            --proof_out="$REPO_ROOT/build/template.proof"
+        check_file_exists "$REPO_ROOT/build/template.proof"
+    fi
+}
+
+run_all() {
+    compile
+    run_assigner
+    build_statement
+    prove
+}
 
 USE_DOCKER=false
 SUBCOMMAND=run_all
@@ -84,7 +145,7 @@ while [[ "$#" -gt 0 ]]; do
         all) SUBCOMMAND=run_all ;;
         compile) SUBCOMMAND=compile ;;
         run_assigner) SUBCOMMAND=run_assigner ;;
-        make_statement) SUBCOMMAND=make_statement ;;
+        build_statement) SUBCOMMAND=build_statement ;;
         prove) SUBCOMMAND=prove ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
