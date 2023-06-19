d3fb1d7d2ac5a150b70d9811cf021bd8ef0a3063
color-typea <colorcode6a@gmail.com>
2023-06-19 23:35:21 +0800

    Minimal test with crypto3::sha256 usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,10 @@
-#include <nil/crypto3/algebra/curves/pallas.hpp>
+#include "lib.hpp"
 
-using namespace nil::crypto3::algebra::curves;
-typename pallas::base_field_type::value_type pow(typename pallas::base_field_type::value_type a, int n) {
-    if (n == 0)
-        return 1;
+using namespace nil::crypto3;
 
-    typename pallas::base_field_type::value_type res = 1;
-    for (int i = 0; i < n; ++i) {
-        res *= a;
-    }
-    return res;
-}
-
-[[circuit]] typename pallas::base_field_type::value_type
-    field_arithmetic_example(typename pallas::base_field_type::value_type a,
-                             typename pallas::base_field_type::value_type b) {
-
-    typename pallas::base_field_type::value_type c = (a + b) * a + b * (a + b) * (a + b);
-    const typename pallas::base_field_type::value_type constant = 0x12345678901234567890_cppui255;
-    return c * c * c / (b - a) + pow(a, 2) + constant;
+[[circuit]] typename hashes::sha2<256>::block_type circuit(
+    hashes::sha2<256>::block_type left,
+    hashes::sha2<256>::block_type right
+) {
+    return hash_pair(left, right);
 }
