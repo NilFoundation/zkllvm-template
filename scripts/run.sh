e74ff2e90ecbc1c8878f63fe1bb718b31a839460
Nick Volynkin <nick.volynkin@gmail.com>
2023-07-12 20:11:33 +0300

    proof-market: update to version 0.0.33
    See:
    * https://github.com/NilFoundation/proof-market-toolchain/releases/tag/v0.0.33
    * https://github.com/NilFoundation/proof-market-toolchain/releases/tag/v0.0.32

--- scripts/run.sh
+++ scripts/run.sh
@@ -15,11 +15,11 @@ REPO_ROOT="$SCRIPT_DIR/.."
 
 # Set image versions in the environment before running this script:
 # export ZKLLVM_VERSION=0.0.58
-# export TOOLCHAIN_VERSION=0.0.31
+# export TOOLCHAIN_VERSION=0.0.33
 
 # If unset, default values will be used:
 echo "using nilfoundation/zkllvm-template:${ZKLLVM_VERSION:=0.0.58}"
-echo "using nilfoundation/proof-market-toolchain:${TOOLCHAIN_VERSION:=0.0.31}"
+echo "using nilfoundation/proof-market-toolchain:${TOOLCHAIN_VERSION:=0.0.33}"
 
 # podman is a safer option for using on CI machines
 if ! command -v podman; then
