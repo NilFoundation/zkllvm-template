ea8b2501bc921ed4660668a160bfbafb855b3538
Nick Volynkin <nick.volynkin@gmail.com>
2023-07-19 09:51:52 +0300

    proof-market-toolchain: upgrade to version 0.0.36

--- scripts/run.sh
+++ scripts/run.sh
@@ -19,7 +19,7 @@ REPO_ROOT="$SCRIPT_DIR/.."
 
 # If unset, default values will be used:
 echo "using nilfoundation/zkllvm-template:${ZKLLVM_VERSION:=0.0.84}"
-echo "using nilfoundation/proof-market-toolchain:${TOOLCHAIN_VERSION:=0.0.35}"
+echo "using nilfoundation/proof-market-toolchain:${TOOLCHAIN_VERSION:=0.0.36}"
 
 # podman is a safer option for using on CI machines
 if ! command -v podman; then
